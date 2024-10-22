8eef4a6c597761deaeab50594323a5e7dca53dc5
M-Laswell <Laswellmic@gmail.com>
2024-10-22 17:57:35 -0600

    setup filesystem for endpoints and everyting

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,11 +11,22 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::register);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/db", this::clearApplication);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
-
         Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+
+    public int port() {
         return Spark.port();
     }
 
