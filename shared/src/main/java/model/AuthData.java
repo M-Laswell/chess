c3ff08069752d15f18373f8477b2bca8d026c764
M-Laswell <Laswellmic@gmail.com>
2024-10-22 18:42:46 -0600

    all model modeling at the basicish level (should all be actually completely done) pps not basic

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,10 +1,47 @@
 package model;
 
-public class AuthData {
-    /*
+import java.util.Objects;
+/*
     Field	Type
     authToken	String
     username	String
     */
 
+public class AuthData {
+    private final String username;
+    private final String authToken;
+
+    AuthData(String authToken, String username) {
+        this.username = username;
+        this.authToken = authToken;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AuthData authData = (AuthData) o;
+        return Objects.equals(username, authData.username) && Objects.equals(authToken, authData.authToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, authToken);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthData{" +
+                "username='" + username + '\'' +
+                ", authToken='" + authToken + '\'' +
+                '}';
+    }
 }
