c3ef832b15033796e6957060fb83932021922bae
M-Laswell <Laswellmic@gmail.com>
2024-10-22 18:16:49 -0600

    setup first handler wohooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -1,4 +1,14 @@
 package handler;
 
-public class RegisterHandler {
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import com.google.gson.Gson;
+
+public class RegisterHandler implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        System.out.println("Made It");
+        return new Gson().toJson("{ 'username':'', 'password':'', 'email':'' }");
+    }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,5 @@
 package server;
-
+import handler.*;
 import spark.*;
 
 
@@ -11,13 +11,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        Spark.post("/user", this::register);
-        Spark.post("/session", this::login);
+        Spark.post("/user", new handler.RegisterHandler());
+        /*Spark.post("/session", this::login);
         Spark.delete("/session", this::logout);
         Spark.get("/game", this::listGames);
         Spark.post("/game", this::createGame);
         Spark.put("/game", this::joinGame);
-        Spark.delete("/db", this::clearApplication);
+        Spark.delete("/db", this::clearApplication);*/
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
