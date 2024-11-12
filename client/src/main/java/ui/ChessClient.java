ac0ccf5cbbbb47a800f18aa45ec078d382706fdc
M-Laswell <Laswellmic@gmail.com>
2024-11-11 18:22:11 -0700

    Committed CHESS'R'US beginnings

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,4 +1,19 @@
 package ui;
 
 public class ChessClient {
+    //private final ServerFacade server;
+    private final String serverUrl;
+
+    public ChessClient(String serverUrl) {
+        //server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String eval(String command){
+        return "evaluating";
+    }
+
+    public String help(){
+        return "help yo self";
+    }
 }
--- client/src/main/java/ui/State.java
+++ client/src/main/java/ui/State.java
@@ -1,4 +1,8 @@
 package ui;
 
-public class State {
+public enum State {
+    SIGNEDOUT,
+    SIGNEDIN,
+    INGAME,
+    OBSERVING
 }
